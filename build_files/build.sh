8cdbef8cd04b9e900a9de8f01ae78b9205f0065c
avereeexyz <211985795+avereeexyz@users.noreply.github.com>
2025-06-08 01:39:56 +0200

    lmfao sureeely this wont work

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux wget curl greetd
+dnf5 install -y @cosmic-desktop-environment
 
 # Use a COPR Example:
 #
@@ -22,3 +23,7 @@ dnf5 install -y tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+
+# REALLy janky shit incoming!
+systemctl disable sddm.service
+systemctl enable greetd.service
